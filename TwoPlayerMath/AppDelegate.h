6d940c0ea44f3a43f09b8f45f49d9fdd3c71f975
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-19 17:19:56 -0800

    Initial Commit

---
+++ TwoPlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayerMath
+//
+//  Created by Chris Dean on 2018-02-19.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoPlayerMath/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TwoPlayerMath
+//
+//  Created by Chris Dean on 2018-02-19.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
